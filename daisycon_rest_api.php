419f46853db38fdeb20e7cefe70f075d180f9744
Samantha Adrichem <s.adrichem@daisycon.com>
2015-01-05 16:31:41 +0100

    Initial setup

---
+++ daisycon_rest_api.php
@@ -0,0 +1,191 @@
+<?php
+
+	/**
+	 * Small Daisycon rest api class
+	 * Simple calls, just check the url in the documentation on https://developers.daisycon.com
+	 *  start with your request method (get, put, post)
+	 *  ignore the "publisher id or advertiser id" in the url
+	 *  camelcase the path, and add the "advertiser id or publisher id" as the first parameter
+	 *  add filter data as an array in the second paramter
+	 *  for example /advertisers/{advertiser_id}/transactions
+	 *  becomes ->getAdvertisersTransactions( ADVERTISERID, array(FILTER_FIELD => VALUE) );
+	 */
+	class DaisyconRestApi
+	{
+
+		const REQUEST_GET = 'GET';
+		const REQUEST_POST = 'POST';
+		const REQUEST_PUT = 'PUT';
+
+		protected $sApiBaseUrl = "https://services.daisycon.com";
+		protected $sUsername;
+		protected $sPassword;
+
+		public function __construct( $sUsername, $sPassword )
+		{
+			$this->sUsername = $sUsername;
+			$this->sPassword = $sPassword;
+		}
+
+		public function __call( $sFunctionName, $aArguments )
+		{
+			$aPath = array();
+			preg_match_all('/((?:^|[A-Z])[a-z]+)/', $sFunctionName, $aPath);
+			$aPath = array_map("strtolower", $aPath[0]);
+			$eRequestType = strtoupper( array_shift($aPath) );
+			if (false === in_array( $eRequestType, array( self::REQUEST_GET, self::REQUEST_POST, self::REQUEST_PUT ) ) )
+			{
+				throw new Exception("Unknown request type: " . $eRequestType );
+			}
+			if (count( $aPath ) > 1 && true === in_array( $aPath[0], array("advertisers", "publishers") ) )
+			{
+				if (count($aArguments) < 1 || (int) $aArguments[0] < 1)
+				{
+					throw new Exception("Advertiser or Publisher service requires an ID as first param");
+				}
+				$aPathTmp = array(
+					array_shift( $aPath ),
+					array_shift( $aArguments )
+				);
+				$aPath = array_merge( $aPathTmp , $aPath );
+			}
+			while (sizeof($aArguments) > 1)
+			{
+				$aPath[] = array_shift($aArguments);
+			}
+			$sRequestUrl = "/" . implode("/", $aPath );
+			$aData = isset($aArguments[0]) ? $aArguments[0] : array();
+			return $this->performCall( $sRequestUrl, $eRequestType, $aData );
+		}
+
+		public function getPublishers()
+		{
+			try
+			{
+				return $this->performCall( "/publishers" );
+			}
+			catch (Exception $oException)
+			{
+				if ($oException->getCode() != 204)
+				{
+					throw $oException;
+				}
+			}
+			return array();
+		}
+
+		public function getPublisherIds()
+		{
+			$oPublishers = $this->getPublishers();
+			$aPublisherIds = array();
+			foreach ($oPublishers as $oPublisher)
+			{
+				$aPublisherIds[] = $oPublisher->id;
+			}
+			return $aPublisherIds;
+		}
+
+		public function getAdvertisers()
+		{
+			try
+			{
+				return $this->performCall( "/advertisers" );
+			}
+			catch (Exception $oException)
+			{
+				if ($oException->getCode() != 204)
+				{
+					throw $oException;
+				}
+			}
+			return array();
+		}
+
+		public function getAdvertiserIds()
+		{
+			$oAdvertisers = $this->getAdvertisers();
+			$aAdvertiserIds = array();
+			foreach ($oAdvertisers as $oAdvertiser)
+			{
+				$aAdvertiserIds[] = $oAdvertiser->id;
+			}
+			return $aAdvertiserIds;
+		}
+
+		public function performCall( $sRequestUrl, $eRequestType = self::REQUEST_GET, $aData = array() )
+		{
+			if (false === stripos( $sRequestUrl, $this->sApiBaseUrl ))
+			{
+				$sRequestUrl =  $this->sApiBaseUrl . $sRequestUrl;
+			}
+			$rCurlHandler = curl_init();
+			$aHeaders = array( 
+				'Authorization: Basic ' . base64_encode( $this->sUsername . ':' . $this->sPassword ) ,
+			);
+			if ( self::REQUEST_GET === $eRequestType )
+			{
+				$sChar = false === stripos($sRequestUrl, "?") ? "?" : "&";
+				foreach ($aData as $sKey => $mValue)
+				{
+					$sRequestUrl .= $sChar . $sKey . "=" . urlencode( $mValue );
+					$sChar = "&";
+				}
+			}
+			else
+			{
+				curl_setopt( $rCurlHandler, CURLOPT_POST, true);
+				curl_setopt( $rCurlHandler, CURLOPT_POSTFIELDS, json_encode($aData) );
+				if ( self::REQUEST_PUT == $eRequestType )
+				{
+					curl_setopt( $rCurlHandler, CURLOPT_CUSTOMREQUEST, "PUT");
+				}
+			}
+			curl_setopt( $rCurlHandler, CURLOPT_URL, $sRequestUrl);
+			curl_setopt( $rCurlHandler, CURLOPT_HTTPHEADER, $aHeaders);
+			curl_setopt( $rCurlHandler, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt( $rCurlHandler, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt( $rCurlHandler, CURLOPT_SSL_VERIFYPEER, 0);
+
+			$sResponse = curl_exec( $rCurlHandler );
+			$iResponseCode = curl_getinfo( $rCurlHandler, CURLINFO_HTTP_CODE);
+			$oResponse = @json_decode($sResponse);
+
+			$sErrorMessage = isset($oResponse->error) ? $oResponse->error : '';
+
+			curl_close( $rCurlHandler );
+
+			switch ( $iResponseCode )
+			{
+				case 200:
+				{
+					return $oResponse;
+				}
+				break;
+
+				case 204:
+				{
+					throw new Exception("204 No content: " . $sErrorMessage, 204);
+				}
+				break;
+
+				case 404:
+				{
+					throw new Exception("404 Service not found: " . $sErrorMessage, 404);
+				}
+				break;
+
+				case 403:
+				{
+					throw new Exception("403 Forbidden: " . $sErrorMessage, 403);
+				}
+				break;
+
+				default:
+				{
+					throw new Exception("Http resonse error " . $iResponseCode . ": " . $sErrorMessage, $iResponseCode);
+				}
+				break;
+			}
+		}
+
+	}
---
+++ example.php
@@ -0,0 +1,55 @@
+	require_once __DIR__ . "/daisycon_rest_api.php";
+
+	/**
+	 * Basic get example
+	 */
+	$oApiCall = new DaisyconRestApi( "USERNAME", "PASSWORD" );
+	foreach ($oApiCall->getAdvertiserIds() as $iAdvertiserId)
+	{
+		$aFilterData = array(
+			'start' => "2014-10-01", 
+			'end' => "2014-10-01" 
+		);
+		$sUrl = '/advertisers/' . $iAdvertiserId . '/transactions';
+		$aFilter = array();
+		foreach ($oApiCall->performCall($sUrl, DaisyconRestApi::REQUEST_GET, $aFilter) as $oTransaction)
+		{
+			var_dump( $oTransaction );break; // stop @ 1
+		}
+	}
+
+	/**
+	 * Basic put example
+	 */
+	$oApiCall = new DaisyconRestApi( "USERNAME", "PASSWORD" );
+	foreach ($oApiCall->getAdvertiserIds() as $iAdvertiserId)
+	{
+		$aPutData = array(
+			'commission' => 12.00,
+			'status' => 'disapproved',
+			'disapproved_reason' => 'duplicate',
+		);
+		$sUrl = '/advertisers/' . $iAdvertiserId . '/transactions/95AFBVUK48AFBLU6V3/7K12';
+		$oResult = $oApiCall->performCall($sUrl, DaisyconRestApi::REQUEST_PUT, $aPutData);
+		var_dump($oResult);
+	}
+
+	/**
+	 * Magic Call not all features supported
+	 */
+	$oApiCall = new DaisyconRestApi( "USERNAME", "PASSWORD" );
+	foreach ($oApiCall->getPublisherIds() as $iPublisherId)
+	{
+		$aFilterData = array(
+			'start' => "2014-10-01",
+			'end' => "2014-10-01"
+		);
+		foreach( $oApiCall->getPublishersTransactions( $iPublisherId, $aFilterData ) as $oTransaction )
+		{
+			var_dump( $oTransaction );break; // stop @ 1
+		}
+	}
+
+
+		
+
